fd76f78be725a42d96e0dd3484bbce56a35b53bf
hemant67890 <godarasubhash337@gmail.com>
2024-11-24 13:02:30 +0530

    Update details.py

--- details.py
+++ details.py
@@ -4,7 +4,7 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", 25218674))
 API_HASH = getenv("API_HASH", "87c231f0e8704795b8239d06965b4351")
-BOT_TOKEN = getenv("BOT_TOKEN", "7198600769:AAGW4WAlCWppTjwTceBxWqDwpWP7UCEWuzI")
+BOT_TOKEN = getenv("BOT_TOKEN", "7883595665:AAGX7Hvl3YJqh8AS3OxJQC9ksEOyyOeitI0")
 OWNER_ID = int(getenv("OWNER_ID", "6805344720"))
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "999741495  20000898623").split()))
 MONGO_URL = getenv("MONGO_DB", "mongodb+srv://daxxop:daxxop@daxxop.dg3umlc.mongodb.net/?retryWrites=true&w=majority")
